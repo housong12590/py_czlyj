b0dfb73147a8676a140ce4eb3d16d8ed43361af5
hous <304536797@qq.com>
2017-08-22 23:23:56 +0800

    111

---
+++ pxbsql.py
@@ -0,0 +1,16 @@
+import pymysql
+
+db = pymysql.connect('127.0.0.1', 'root', '123546', 'czlyj', charset='utf8', use_unicode=True)
+
+cursor = db.cursor()
+create_sql = """CREATE TABLE pxb (
+         name  CHAR(255),
+         person  CHAR(255),
+         phone CHAR(255)"""
+
+insert_sql = """INSERT INTO pxb(name,person, phone) VALUES (%s, %s, %s)"""
+
+
+def insert(d):
+    cursor.executemany(insert_sql, d)
+    db.commit()
--- qxb.py
+++ qxb.py
@@ -3,7 +3,7 @@ import urllib.parse
 import urllib.error
 import json
 import urllib.parse
-import mysql
+import pxbsql
 from config import *
 from bs4 import BeautifulSoup
 
@@ -31,71 +31,30 @@ def get_page_content(key, province="JS", city="1", page="1"):
 
 
 def parse_html_content(html):
-    data = []
     soup = BeautifulSoup(html, "lxml")
-    try:
-        tbody = soup.find("tbody")
-        tr = tbody.find_all("tr")
-        for t in tr:
-            td_list = t.find_all("td")
-            info_td = td_list[1]
-            # 企业名称
-            company_name = info_td.find("a").get_text()
-            print(company_name)
-
-            # 法人代表
-            p_list = info_td.find_all("p")
-            legal_name = p_list[0].find("a").get_text()
-            print(legal_name)
-
-            span_list = p_list[0].find_all("span")
-            # 注册资本
-            money = span_list[0].get_text()[5:]
-            print(money)
-
-            # 成立时间
-            time = span_list[1].get_text().strip()[5:]
-            print(time)
-
-            # 电话号码
-            phone = p_list[1].get_text().strip('\n').split('\n')[0][3:]
-            print(phone)
-
-            # 邮箱
-            email = p_list[1].find("span").get_text()[3:]
-            print(email)
-
-            # 地址
-            address = p_list[2].get_text().strip()[3:]
-            print(address)
-
-            # # 经营范围
-            # range = p_list[3].get_text()
-            # print(range)
-
-            # 企业状态
-            status_td = td_list[2]
-            status = status_td.find("span").get_text()
-            print(status)
-
-            exp = (company_name, legal_name, money, time, phone, email, address, status)
-            data.append(exp)
-    except Exception as e:
-        print("-----")
-        pass
+    data = []
+    i = 0
+    for div in soup.findAll("div", {'class': 'col-xs-24 padding-v-1x margin-0-0x border-b-b4 company-item'}):
+        name = div.find("a", {"title": '点击查看公司详情'}).get_text()
+        person = div.find("div", {'class': 'legal-person'}).get_text()[6:]
+        phone = ""
+        try:
+            phone = div.find("span", {'class': 'margin-r-1x'}).get_text()
+        except Exception:
+            phone = ""
+        i += 1
+        print(i)
+        data.append((name, person, phone))
     return data
 
 
 def main():
     p = 0
-    for i in range(1):
-        html = get_page_content("建筑", '32', '3201', str(19))
-        print(html)
-        f = open("test.html", "wb")
-        f.write(html)
-        f.close()
-        # data = parse_html_content(html)
-        # mysql.insert(data)
+    for i in range(54, 500):
+        html = get_page_content("食品", '32', '3201', str(i))
+        data = parse_html_content(html)
+
+        pxbsql.insert(data)
 
 
 if __name__ == '__main__':
