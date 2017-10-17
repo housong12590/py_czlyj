247c43c0279318a2ca1142bef0815f686a68757f
housong12590 <304536797@qq.com>
2017-10-17 20:56:43 +0800

    qweqwe

--- qiniudemo.py
+++ qiniudemo.py
@@ -1,16 +1,29 @@
 from qiniu import Auth, put_file, etag, urlsafe_base64_encode
 import qiniu.config
+import http
 import os
+from urllib import request
 
 access_key = 'T3MasEjXJ-bNYueJB5Pc8zIulcbdOfJK1CW6oDUS'
 secret_key = 'xamgaJxWklW4NFaM5INYaVqsUYT-CubVU6zc8yLf'
 bucket_name = 'pss123546'
 
-# os.listdir('C:/Users/Administrator/Desktop/qiniuimage')
 parent_path = 'C:/Users/Administrator/Desktop/qiniuimage'
 local_files = os.listdir(parent_path)
-request = Auth(access_key, secret_key)
-for file in local_files:
-    token = request.upload_token(bucket_name, file)
-    ret, info = put_file(token, file, parent_path + "/" + file)
-    print(info)
+r = Auth(access_key, secret_key)
+# for file in local_files:
+#     token = request.upload_token(bucket_name, file)
+#     ret, info = put_file(token, file, parent_path + "/" + file)
+#     print(info)
+
+
+host = 'http://rs.qbox.me'
+url = host + '/buckets'
+token = r.token_of_request(url)
+authorization = 'QBox {}'.format(token)
+headers = {
+    'Authorization': authorization
+}
+req = request.Request(url, headers=headers)
+result = request.urlopen(req).read()
+print(result)
